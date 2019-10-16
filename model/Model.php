78207043e6bb39a12bfd2011b4c629b649620fae
David Binaud <david.binaud@etu.umontpellier.fr>
2019-10-16 11:34:37 +0200

    modif du Model.php pour adapter au nom des tables

---
+++ model/Model.php
@@ -0,0 +1,57 @@
+<?php
+	//require_once '../config/Conf.php';
+	require_once (File::build_path(array("config","Conf.php"))); // chargement du modèle
+	class Model {
+
+		public static $pdo;
+
+
+		public static function Init(){
+
+			$hostname = Conf::getHostname();
+			$database_name = Conf::getDatabase();
+			$login = Conf::getLogin();
+			$password = Conf::getPassword();
+
+			try{
+				// Connexion à la base de données            
+				// Le dernier argument sert à ce que toutes les chaines de caractères 
+				// en entrée et sortie de MySql soit dans le codage UTF-8
+				self::$pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $login, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+   
+				// On active le mode d'affichage des erreurs, et le lancement d'exception en cas d'erreur
+				self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				
+			} catch(PDOException $e) {
+  				if (Conf::getDebug()) {
+    				echo $e->getMessage(); // affiche un message d'erreur
+  				} else {
+    				echo 'Une erreur est survenue <a href=""> retour a la page d\'accueil </a>';
+				  }
+				die();
+			}
+
+		}
+
+
+
+	public static function selectAll(){
+
+		$object_name = static::$object;
+		$class_name = "Model" . ucfirst($object_name);
+
+		
+		$table_name = "eCom_" . static::$object;
+
+
+     	$rep = self::$pdo->query("SELECT * FROM {$table_name}");
+
+      	$rep->setFetchMode(PDO::FETCH_CLASS, $class_name);
+
+      	return  $rep->fetchAll();
+    }
+
+
+	}
+	Model::Init();
+?>
