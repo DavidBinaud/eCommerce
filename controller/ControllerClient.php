b0713539d3d4fc0e9758e7a7c70e919ba968c4ed
David Binaud <david.binaud@etu.umontpellier.fr>
2019-11-11 14:08:31 +0100

    Controller Client: rectification des duplications de require

--- controller/ControllerClient.php
+++ controller/ControllerClient.php
@@ -25,18 +25,15 @@
 			if(isset($_GET['id'])){
 				$c = ModelClient::select($_GET['id']);
 				if($c == false){
-					
 					$view='error'; $pagetitle='ErreurClient'; $errorType = "Read d'un Client: id fourni non existant";
-					require (File::build_path(array("view","view.php")));
 				}else
 				{
 					$view='detail'; $pagetitle='Detail Client';
-					require (File::build_path(array("view","view.php")));
 				}
 			}else{
-				$view='error'; $pagetitle='ErreurClient'; $errorType = "Read d'un Client: Pas d'id fourni";
-				require (File::build_path(array("view","view.php")));
+				$view='error'; $pagetitle='ErreurClient'; $errorType = "Read d'un Client: Pas d'id fourni";	
 			}
+			require (File::build_path(array("view","view.php")));
 		}
 
 
@@ -46,18 +43,16 @@
 				$c = ModelClient::select($_GET['id']);
 				if($c == false){
 					$view='error'; $pagetitle='ErreurProduit'; $errorType = "Delete d'un Client: id fourni non existant";
-					require (File::build_path(array("view","view.php")));
 				}else{
 					ModelClient::delete($_GET['id']);
 					$tab_c = ModelClient::selectAll();
 
 					$view='delete'; $pagetitle='Suppresion Clients';
-					require (File::build_path(array("view","view.php")));
 				}
 			}else{
 				$view='error'; $pagetitle='ErreurClients'; $errorType = "Delete d'un Clients: Pas d'id fourni";
-				require (File::build_path(array("view","view.php")));
 			}
+			require (File::build_path(array("view","view.php")));
 		}
 
 
@@ -85,16 +80,14 @@
 				$c = new ModelClient($_GET);
 				if(ModelClient::save($c) == false){
 					$view='error'; $pagetitle='Erreur de Création'; $errorType = 'Created Client: id fourni déjà existant';
-					require (File::build_path(array("view","view.php")));
 				}else{
 					$tab_c = ModelClient::selectAll();
 					$view='created'; $pagetitle='Création Reussie';
-					require (File::build_path(array("view","view.php")));
 				}
 			}else{
 				$view='error'; $pagetitle='ErreurClient'; $errorType = "Create d'un Client: Problème de paramètres";
-				require (File::build_path(array("view","view.php")));
 			}
+			require (File::build_path(array("view","view.php")));
 		}
 
 
@@ -105,10 +98,9 @@
 
 				if($c == false){
 					$view='error'; $pagetitle='Erreur MAJ'; $errorType = 'update Client: id fourni non existant';
-					require (File::build_path(array("view","view.php")));
+					
 				}
 				else{
-
     				$cId = htmlspecialchars($c->get("id"));
 					$cNom = htmlspecialchars($c->get("nom"));
 					$cPrenom = htmlspecialchars($c->get("prenom"));
@@ -119,13 +111,12 @@
     				$cAction = "update";
 
 					$view='update'; $pagetitle='Mise A Jour';
-					require (File::build_path(array("view","view.php")));
 				}
 			}
 			else{
 				$view='error'; $pagetitle='Erreur MAJ'; $errorType = 'update Client: Problème de paramètres';
-					require (File::build_path(array("view","view.php")));
 			}
+			require (File::build_path(array("view","view.php")));
 		}
 
 
@@ -136,7 +127,7 @@
 				$c = ModelClient::select($_GET['id']);
 				if($c == false){
 					$view='error'; $pagetitle='Erreur MAJ'; $errorType = 'update Client: id fourni non existant';
-					require (File::build_path(array("view","view.php")));
+					
 				}
 				else{
 					$data = array(
@@ -148,18 +139,18 @@
 						"adresse" => $_GET['adresse'],
 						"dateDeNaissance" => $_GET['dateDeNaissance']
 					);
-	
-					ModelClient::update($data);
-	
+					if (ModelClient::update($data)) {
 						$tab_c = ModelClient::selectAll();
 						$view='updated'; $pagetitle='Mise A Jour';
-					require (File::build_path(array("view","view.php")));
+					}else{
+						$view='error'; $pagetitle='Erreur MAJ'; $errorType = 'updated Client: Problème de maj rencontré';
+					}
 				}
 			}
 			else{
 				$view='error'; $pagetitle='Erreur MAJ'; $errorType = 'updated Client: Problème de paramètres';
-				require (File::build_path(array("view","view.php")));
 			}
+			require (File::build_path(array("view","view.php")));
 			
 		}
 	
--- view/client/update.php
+++ view/client/update.php
@@ -17,37 +17,37 @@
 		<legend>Mon formulaire de <?php echo $titreForm;?>:</legend>
 	    
 		<p>
-	 	 	<label for="id_id">Id</label> :
+	 	 	<label for="id_id">Id:</label> 
 			<input type="text" value="<?php echo $cId;?>" name="id" id="id_id" <?php echo $loginParam;?>/>
 		</p>
 
 		<p>
-	  		<label for="nom_id">Nom</label> :
+	  		<label for="nom_id">Nom:</label> 
 	  		<input type="text" value="<?php echo $cNom;?>" name="nom" id="nom_id" required/>
 		</p>
 
 		<p>
-	 		<label for="prenom_id">Prenom</label> :
+	 		<label for="prenom_id">Prenom:</label> 
 	 	 	<input type="text" value="<?php echo $cPrenom;?>" name="prenom" id="prenom_id" required/>
 		</p>
 
 		<p>
-			<label for="ville_id">Ville</label> :
+			<label for="ville_id">Ville:</label> 
 			<input type="text" value="<?php echo $cVille;?>" name="ville" id="ville_id" required/>
 		</p>
 
 		<p>
-			<label for="pays_id">Pays</label> :
+			<label for="pays_id">Pays:</label> 
 			<input type="text" value="<?php echo $cPays;?>" name="pays" id="pays_id"/>
 		</p>
 
 		<p>
-			<label for="adresse_id">Adresse</label> :
+			<label for="adresse_id">Adresse:</label> 
 			<input type="text" value="<?php echo $cAdresse;?>" name="adresse" id="adresse_id"/>
 		</p>
 
 		<p>
-			<label for="dateDeNaissance_id">Date de Naissance</label> :
+			<label for="dateDeNaissance_id">Date de Naissance:</label> 
 			<input type="text" value="<?php echo $cDateDeNaissance;?>" name="dateDeNaissance" id="dateDeNaissance_id"/>
 		</p>
 
