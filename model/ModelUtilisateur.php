48f4ce332a80fbf4ee7637170b1ba4d9a7815882
DavidBinaud <david.binaud@etu.umontpellier.fr>
2019-11-20 10:54:43 +0100

    rajout check password et differents attributs (nonce isadmin email)

--- model/ModelUtilisateur.php
+++ model/ModelUtilisateur.php
@@ -4,7 +4,9 @@
 
   class ModelUtilisateur extends Model{
            
-    private $id;
+    private $login;
+    private $mdp;
+    private $email;
     private $nom;
     private $prenom;
     private $ville;
@@ -14,7 +16,7 @@
     private $is_admin;
     private $nonce;
     protected static $object = 'utilisateur';
-    protected static $primary='id';
+    protected static $primary='login';
               
     //getter générique
     public function get($nom_attribut){
@@ -31,11 +33,13 @@
     // Si un argument optionnel n'est pas fourni,
     //   alors il prend la valeur par défaut, NULL dans notre cas
     public function __construct($data = NULL) {
-      if (!is_null($data['id']) && !is_null($data['nom']) && !is_null($data['prenom']) && !is_null($data['ville']) && !is_null($data['pays']) && !is_null($data['adresse']) && !is_null($data['dateDeNaissance']) && !is_null($data['is_admin']) && !is_null($data['nonce'])) {
+      if (!is_null($data['login']) && !is_null($data['mdp']) && !is_null($data['email']) && !is_null($data['nom']) && !is_null($data['prenom']) && !is_null($data['ville']) && !is_null($data['pays']) && !is_null($data['adresse']) && !is_null($data['dateDeNaissance']) && !is_null($data['is_admin']) && !is_null($data['nonce'])) {
         // Si aucun de $m, $c et $i sont nuls,
         // c'est forcement qu'on les a fournis
         // donc on retombe sur le constructeur à 3 arguments
-        $this->id = $data['id'];
+        $this->login = $data['login'];
+        $this->mdp = $data['mdp'];
+        $this->email = $data['email'];
         $this->nom = $data['nom'];
         $this->prenom = $data['prenom'];
         $this->ville = $data['ville'];
@@ -43,13 +47,40 @@
         $this->adresse = $data['adresse'];
         $this->dateDeNaissance = $data['dateDeNaissance'];
         $this->is_admin = $data['is_admin'];
-        $this->nonce = $data['dateDeNaissance'];
+        $this->nonce = $data['nonce'];
       }
     }
 
 
     public function get_object_vars() {
         return get_object_vars($this);
+    }
+
+
+
+    public static function checkPassword($login,$mot_de_passe_chiffre){
+      $sql ="SELECT COUNT(*) FROM utilisateur WHERE login=:login AND mdp=:motdepasse";
+
+
+      $req_prep = Model::$pdo->prepare($sql);
+
+      $values = array(
+        'login' => $login,
+        'motdepasse' => $mot_de_passe_chiffre
+      );
+
+
+      $req_prep->execute($values);
+
+      $req_prep->setFetchMode(PDO::FETCH_ASSOC);
+
+      $resultat = $req_prep->fetchAll();
+      
+      if($resultat[0]['COUNT(*)'] == '1'){
+        return true;
+      }
+      return false;
+    
     }
 
  }
