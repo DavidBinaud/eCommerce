f4970492f171713838092edc2e30660ec916deb4
DavidBinaud <david.binaud@etu.umontpellier.fr>
2019-11-20 10:53:14 +0100

    Nav modulable en fonction de l'etat de connexion et si admin ou non

--- view/view.php
+++ view/view.php
@@ -5,16 +5,35 @@
         <title><?php echo $pagetitle; ?></title>
     </head>
     <body>
+    <?php
 
+    if(Session::is_admin()){
+        echo '
     	<header>
     		<div class="nav">
 			  <a href="index.php?action=readAll&controller=produit">Gestion Produits</a>
-			  <a href="index.php?action=readAll&controller=client">Gestion Clients</a>
+			  <a href="index.php?action=readAll&controller=utilisateur">Gestion Utilisateurs</a>
 			  <a href="index.php?action=readAll&controller=commande">Gestion Commandes</a>
+              <a href="index.php?action=deconnect&controller=utilisateur">Deconnexion</a>
 			</div>
-    	</header>
+    	</header>';
+    }else{
+        echo '
+        <header>
+            <div class="nav">
+              <a href="index.php?action=readAll&controller=produit">Produits</a>';
+        if(!isset($_SESSION['login'])){
+            echo '<a href="index.php?action=connect&controller=utilisateur">Connexion</a>';
+        }else{
+            $loginURL = rawurlencode($_SESSION['login']);
+            echo "<a href='index.php?action=read&controller=utilisateur&login=$loginURL'>Compte</a>";
+            echo '<a href="index.php?action=deconnect&controller=utilisateur">Deconnexion</a>';
+        }
 
-	<?php
+        echo    '</div>
+        </header>';
+    }
+	
 		// Si $controleur='voiture' et $view='list',
 		// alors $filepath="/chemin_du_site/view/voiture/list.php"
 		$filepath = File::build_path(array("view",static::$object, "$view.php"));
