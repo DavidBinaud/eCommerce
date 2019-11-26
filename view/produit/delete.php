d6785622a5520a8647eaebc038412466b909fbff
Manelphe Théo <theo.manelphe@etu.umontpellier.fr>
2019-11-26 11:17:21 +0100

    ajout pas reset si erreur formulaire

--- view/produit/delete.php
+++ view/produit/delete.php
@@ -1,5 +1,5 @@
 <?php
     $pid = htmlspecialchars($_GET['id']);
-    echo '<p> Le produit d\'id ' . $pid . " a bien été supprimée.</p>";
+    echo '<p> Le produit d\'id ' . $pid . " a bien été supprimé.</p>";
     require File::build_path(array("view",static::$object,"list.php"));
 ?>
