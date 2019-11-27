c81faf25a26d2569bd9a8f029f9855f248844e00
David Binaud <david.binaud@etu.umontpellier.fr>
2019-11-27 11:15:00 +0100

    retrait des fichiers materialize locaux

--- view/view.php
+++ view/view.php
@@ -4,8 +4,8 @@
         <link rel="stylesheet" type="text/css" href="css/styles.css">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-<link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet"> 
         <meta charset="UTF-8">
         <title><?php echo $pagetitle; ?></title>
     </head>
