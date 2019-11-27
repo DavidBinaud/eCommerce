14b13fc5aaed12451b3cbf1056974fc513d12ef0
Corentin Sarlin <corentin.sarlin@etu-umontpellier.fr>
2019-11-27 11:11:22 +0100

    modification de l'affichage des produits + correction affichage background

--- view/produit/list.php
+++ view/produit/list.php
@@ -3,14 +3,12 @@
 		$pid = htmlspecialchars($p->get("id"));
 		$pidURL = rawurlencode($p->get("id"));
 		$pnom = htmlspecialchars($p->get("nom"));
-		$pimg = htmlspecialchars($p->get("pathImgProduit"));
 		$nationalite = htmlspecialchars($p->get("nationalite"));
 		$type = htmlspecialchars($p->get("type"));
+		$path = htmlspecialchars($p->get("pathImgProduit"));
 		
 
-		echo "<p> $type de nationnalité $nationalite, le
-		<a href=index.php?controller=produit&action=read&id=$pidURL>{$pnom}</a>
-		 ";
+		echo "<p><a href=index.php?controller=produit&action=read&id=$pidURL><img alt='{$pnom}' src='{$path}'></a>";
 		if(Session::is_admin()){
 			echo "(<a href=index.php?controller=produit&action=delete&id=$pidURL>Supprimer</a>)";
 		}
